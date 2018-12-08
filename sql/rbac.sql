c9d4a0fa9f6d241ef664959f6d4deb43ae40c152
luan@zhang <luan@zhang>
2018-12-08 15:40:40 +0800

    提交sql脚本文件

---
+++ sql/rbac.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1_3306
+Source Server Version : 50525
+Source Host           : 127.0.0.1:3306
+Source Database       : rbac
+
+Target Server Type    : MYSQL
+Target Server Version : 50525
+File Encoding         : 65001
+
+Date: 2018-12-08 15:28:37
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
